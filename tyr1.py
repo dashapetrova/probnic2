c7147b270b98054b52a0c2d5c8839eae9243b81b
dashapetrova <petrova.s.daria@gmail.com>
2016-12-13 16:33:04 +0300

    Add files via upload

---
+++ tyr1.py
@@ -0,0 +1,13 @@
+def f1():
+    text=open("text.txt")
+    s=text.read().lower().split(" ")
+    text.close()
+    return s
+s=f1()
+print(s)
+i=0
+for word in s:
+    i+=1
+print("number of words = ",i)
+        
+    
